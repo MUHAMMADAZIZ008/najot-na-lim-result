3c266465e5627fcd2703a6d95183b5fe7dd3cae6
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2024-12-13 16:56:31 +0500

    update

--- darslar/4-oy/9-dars/lesson9/src/users/repasitory/user.repasitory.ts
+++ darslar/4-oy/9-dars/lesson9/src/users/repasitory/user.repasitory.ts
@@ -4,7 +4,7 @@ import { UpdateUserDto } from '../dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../entities/user.entity';
-import { AlreadyExsist, UserNotFound } from 'src/exception/exception';
+import { AlreadyExsist } from 'src/exception/exception';
 
 @Injectable()
 export class UsersRopsitory {
