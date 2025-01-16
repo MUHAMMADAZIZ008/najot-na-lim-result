2ec06c9cb30bd17e7fcd83cb6999c1be458337a1
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2025-01-16 14:41:24 +0500

    add

---
+++ darslar/5-oy/9-dars/todo/src/todos/todos.gateway.ts
@@ -0,0 +1,38 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+} from '@nestjs/websockets';
+import { TodosService } from './todos.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+
+@WebSocketGateway()
+export class TodosGateway {
+  constructor(private readonly todosService: TodosService) {}
+
+  @SubscribeMessage('createTodo')
+  create(@MessageBody() createTodoDto: CreateTodoDto) {
+    return this.todosService.create(createTodoDto);
+  }
+
+  @SubscribeMessage('findAllTodos')
+  findAll() {
+    return this.todosService.findAll();
+  }
+
+  @SubscribeMessage('findOneTodo')
+  findOne(@MessageBody() id: number) {
+    return this.todosService.findOne(id);
+  }
+
+  @SubscribeMessage('updateTodo')
+  update(@MessageBody() updateTodoDto: UpdateTodoDto) {
+    return this.todosService.update(updateTodoDto.id, updateTodoDto);
+  }
+
+  @SubscribeMessage('removeTodo')
+  remove(@MessageBody() id: number) {
+    return this.todosService.remove(id);
+  }
+}
